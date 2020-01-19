b56e23c9b189b4c53c8a36ff0bdaa5e0c20c6d3b
Dan Gallagher <dagallagher221@gmail.com>
2020-01-19 14:56:06 -0500

    fixed errors with app functions, need to fix image css and grid

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import Header from "./Components/Header"
 import Wrapper from './Components/Wrapper';
+import Grid from "./Components/Grid"
 import './App.css';
 
 class App extends Component {
